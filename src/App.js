25031b7f17e89f181fb92fa24b4775c898f76e61
Sirichai <13_oy@hotmail.co.th>
2018-07-16 22:47:14 +0700

    update update task && edit interface

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-responsive-modal';
 import './App.css';
-
-import TodoList from './component/todolist'
+import TodoList from './component/todolist';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
-    this.state = {
-      items: [],
-      text: "",
-      chkall:false
 
-    };
-    
+    const data = JSON.parse(localStorage.getItem('Session_todo'));
+    console.log(data)
+    if (data) {
+      this.state = data;
+    } else {
+      this.state = {
+        items: [],
+        text: "",
+        des:"",
+        date:"",
+        chkall: false,
+        open: false,
+        complete:0
+      };
+    }
+
     this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleDesChange = this.handleDesChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
     this.handleAddItem = this.handleAddItem.bind(this);
     this.markItemCompleted = this.markItemCompleted.bind(this);
     this.handleDeleteItem = this.handleDeleteItem.bind(this);
     this.Chackall = this.Chackall.bind(this);
     this.Delete_list = this.Delete_list.bind(this);
+    this.UpdateItem = this.UpdateItem.bind(this);
+    this.onOpenModal = this.onOpenModal.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
   }
+
+  onCloseModal() {
+    this.setState({ open: false });
+  }
+
+  onOpenModal() {
+    this.setState({ open: true });
+  }
+
 
   handleTextChange(event) {
     this.setState({
@@ -29,60 +52,73 @@ class App extends Component {
     });
   }
 
+  handleDesChange(event) {
+    this.setState({
+      des: event.target.value
+    });
+  }
+
+  handleDateChange(event) {
+    this.setState({
+      date: event.target.value
+    });
+  }
+
   handleAddItem(event) {
     event.preventDefault();
-
     var newItem = {
       id: Date.now(),
       text: this.state.text,
+      des:this.state.des,
+      date:this.state.date,
       done: false
     };
-  
     this.setState((prevState) => ({
       items: prevState.items.concat(newItem),
-      text: ""
+      text: "",
+      des:"",
+      date:"",
+      complete:this.state.complete + 1
     }));
+    this.onCloseModal();
   }
 
   markItemCompleted(itemId) {
-    // console.log(this.state,itemId)
     var updatedItems = this.state.items.map(item => {
       if (itemId === item.id)
         item.done = !item.done;
       return item;
     });
 
-    if(this.state.chkall === true){
+    if (this.state.chkall === true) {
       this.setState({
         items: [].concat(updatedItems),
         chkall: false
-      }); 
-    }else{
+      });
+    } else {
       this.setState({
         items: [].concat(updatedItems)
       });
     }
-    
   }
 
-  Chackall(){
-    if(this.state.chkall === true){
+  Chackall() {
+    if (this.state.chkall === true) {
       this.setState({
         chkall: false
       });
-    }else{
+    } else {
       this.setState({
         chkall: true
       });
     }
-    
+
     var CheckItems = this.state.items.map(item => {
-      // console.log(this.state)
-      if(this.state.chkall === true){
+      if (this.state.chkall === true) {
         item.done = false;
-      }else{
+      } else {
         item.done = true;
-      }  
+      }
       return item;
     });
     this.setState({
@@ -90,75 +126,102 @@ class App extends Component {
     });
   }
 
+  UpdateItem(itemId,itemArray) {
+    this.setState({items: this.state.items.map(item =>item.id === itemId
+       ? Object.assign(itemArray) : item )
+      });
+  }
+
   handleDeleteItem(itemId) {
     var updatedItems = this.state.items.filter(item => {
       return item.id !== itemId;
     });
-    
 
     this.setState({
       items: [].concat(updatedItems)
     });
   }
 
-  Delete_list(){
-   if(this.state.items.length === 0){
-    alert('No row selected.')
-   }else{
-    var list_Items = this.state.items.filter(item => item.done === false);
-    
-    if(list_Items.length !== this.state.items.length){
-      
-      if (window.confirm('Are you sure you want to delete ?')) { 
-        this.setState({
-          items: [].concat(list_Items)
-        });
-      }
-      
-    }else{
+  Delete_list() {
+    if (this.state.items.length === 0) {
       alert('No row selected.')
+    } else {
+      var list_Items = this.state.items.filter(item => item.done === false);
+
+      if (list_Items.length !== this.state.items.length) {
+        if (window.confirm('Are you sure you want to delete ?')) {
+          this.setState({
+            items: [].concat(list_Items)
+          });
+        }
+      } else {
+        alert('No row selected.')
+      }
     }
-   }
-    
+
   }
- 
-  render() {
+  
+  componentDidUpdate() {
     
+    localStorage.setItem('Session_todo', JSON.stringify(this.state));
+  }
+
+  render() {
+    const { open } = this.state;
+    const complete = this.state.items.filter(item =>item.done === true).length;
     return (
       <div>
-      <header className="App-header" style={{marginBottom : "20px"}}>
+        <header className="App-header" style={{ marginBottom: "20px" }}>
           <h3 className="apptitle">MY TO DO LIST</h3>
-      </header>
-      <div className="col-md-8 col-md-offset-2">
-        <form className="row">
-           <div className="col-md-offset-2 col-md-2 nopaddingleft-right width_checkall">
-            <input type="checkbox" className="form-check-input" onClick={this.Chackall} checked={this.state.chkall === true}/> 
+        </header>
+        <div className="col-md-8 col-md-offset-2">
+          <div className="row">
+            <div className="col-md-offset-2 col-md-3 count_todocomplete"><strong>{complete >= 0 ? complete: 0} Completed</strong></div>
+            <div className="col-md-5" align="right">
+              <button onClick={this.onOpenModal} className="btn"><i className="fa fa-lg fa-plus"></i></button>
+            </div>
+          </div>
+          <div className="row dataContent">
+            <div className="col-md-8 col-md-offset-2">
+              <TodoList items={this.state.items} onItemCompleted={this.markItemCompleted} onUpdateItem={this.UpdateItem} onDeleteItem={this.handleDeleteItem} />
+            </div>
+          </div>
+          {/* <div className="col-md-offset-2 col-md-2 nopaddingleft-right width_checkall">
+            <input type="checkbox" className="form-check-input" onChange={this.Chackall} checked={this.state.chkall === true} />
             <span className="checkall">Check all</span>
           </div>
           <div className="col-md-1 deletelist" onClick={this.Delete_list}>
-                <i className="fa fa-trash-o" aria-hidden="true"> Delete</i>
-          </div>
-          <div className="col-md-4">
-            <input type="text" className="form-control" onChange={this.handleTextChange} value={this.state.text} placeholder="กรุณากรอกรายการ"/>
-          </div>
-          <div className="col-md-2">
-            <button className="btn btn-primary" onClick={this.handleAddItem} disabled={!this.state.text}>Add</button>
-          </div>
-        </form>
-        <div className="row dataContent">
-          <div className="col-md-8 col-md-offset-2">
-            <TodoList items={this.state.items} onItemCompleted={this.markItemCompleted} onDeleteItem={this.handleDeleteItem} />
-          </div>
+            <i className="fa fa-trash-o" aria-hidden="true"> Delete</i>
+          </div> */}
         </div>
-      </div>
+        <Modal open={open} onClose={this.onCloseModal}>
+          <form className="submit_addtodo" onSubmit={this.handleAddItem}>
+            <div className="modal-header">
+              <h4 className="modal-title">Add New Todo</h4>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <label htmlFor="user">Title:</label>
+                <input type="text" className="form-control" onChange={this.handleTextChange} value={this.state.text} placeholder="Please input Title"/>
+              </div>
+              <div className="form-group">
+                <label htmlFor="des">Description:</label>
+                <textarea className="form-control" onChange={this.handleDesChange} value={this.state.des} placeholder="Please input Description">{this.state.des}</textarea>
+              </div>
+              <div className="form-group">
+                <label htmlFor="date">Date:</label>
+                <input type="date" className="form-control" onChange={this.handleDateChange} value={this.state.date}/>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-primary" disabled={!this.state.text}>Add</button>
+            </div>
+          </form>
+        </Modal>
       </div>
     );
   }
 }
-
-
-
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
--- src/component/todolist.js
+++ src/component/todolist.js
@@ -1,107 +1,172 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import uuid from 'node-uuid';
+import Modal from 'react-responsive-modal';
 
 class TodoList extends Component {
-    render() {
-      return (
-        <div className="todolist">
-          {this.props.items.map(item => (
-            <TodoItem key={item.id} id={item.id} text={item.text} completed={item.done} onItemCompleted={this.props.onItemCompleted} onDeleteItem={this.props.onDeleteItem} />
-          ))}
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="todolist">
+        {this.props.items.map(item => (
+          <TodoItem key={uuid()} id={item.id} text={item.text} des={item.des} date={item.date} completed={item.done} onItemCompleted={this.props.onItemCompleted} onUpdateItem={this.props.onUpdateItem} onDeleteItem={this.props.onDeleteItem} />
+        ))}
+      </div>
+    );
+  }
+}
+
+class TodoItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Isclick: false,
+      text: this.props.text,
+      oldtext: this.props.text,
+      des: this.props.des,
+      date: this.props.date,
+      id: this.props.id,
+      open: false
+    };
+
+    this.markCompleted = this.markCompleted.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.handinfo = this.handinfo.bind(this);
+    this.handleTextUpdate = this.handleTextUpdate.bind(this);
+    this.handleDesUpdate = this.handleDesUpdate.bind(this);
+    this.handleDateUpdate = this.handleDateUpdate.bind(this);
+    this.UpdateItem = this.UpdateItem.bind(this);
+    this.CancleItem = this.CancleItem.bind(this);
+    this.onOpenModal = this.onOpenModal.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
   }
 
-  class TodoItem extends Component {
-    constructor(props) {
-      super(props);
-        
-      this.state = {
-        Isclick: false,
-        text : this.props.text,
-        oldtext : this.props.text,
-        id : this.props.id,
-      };
-
-      this.markCompleted = this.markCompleted.bind(this);
-      this.deleteItem = this.deleteItem.bind(this);
-      this.handinfo = this.handinfo.bind(this);
-      this.handleTextUpdate = this.handleTextUpdate.bind(this);
-      this.UpdateItem = this.UpdateItem.bind(this);
-      this.CancleItem = this.CancleItem.bind(this);
-    }
-    markCompleted(event) {
-      this.props.onItemCompleted(this.props.id);
-    }
-    deleteItem(event) {
-        if (window.confirm('Are you sure you want to delete ?')) { 
-          this.props.onDeleteItem(this.props.id);
-        } 
-    }
-    handinfo(event){
-        this.setState({
-          Isclick: true
-        });
-    }
-
-    CancleItem(event){
-      // event.preventDefault();
-      this.setState({
-        Isclick: false,
-        text: this.state.oldtext
-      });
-
-    //   console.log(this.state.Isclick)
-    }
-
-    UpdateItem(event){
-      event.preventDefault();
-      this.setState({
-        Isclick: false,
-        text:this.state.text,
-        oldtext:this.state.text
-      });
-    }
-
-    handleTextUpdate(event){
-      this.setState({
-        text: event.target.value
-      }); 
-    }
-    
-    render() {
-      var itemClass = "row form-check todoitem " + (this.props.completed ? "done" : "undone");
-      return (
-        <div id="box" className={itemClass} ref={li => this._listItem = li }>
-          <div className="col-md-1">
-            <input type="checkbox" className="form-check-input" onChange={this.markCompleted} checked={this.props.completed} /> 
-          </div>
-          <div className="col-md-10">  
-              {this.state.Isclick === true ? 
-                 <form className="row" onSubmit={this.UpdateItem}>
-                    <div className="col-md-8">
-                      <input type="text" className="form-control" onChange={this.handleTextUpdate} value={this.state.text} placeholder="กรุณากรอกรายการ"/>
-                    </div>
-                    <div className="col-md-4 btn-box">
-                      <button disabled={!this.props.text} className="btn btn-primary">
-                        <i className='fa fa-check-circle-o' aria-hidden='true'></i>
-                      </button>
-                        
-                      <a onClick={this.CancleItem} className="btn btn-danger btn-paddingleft">
-                        <i className="fa fa-times-circle" aria-hidden="true"></i>
-                      </a>
-                    </div>    
-                </form>  
-                :<label className="form-check-label"><a onClick={this.handinfo} id="text_todo">{this.state.text}</a> </label> }
-          </div>
-          <div className="col-md-1">
-              <button type="button" className="btn btn-danger btn-sm" onClick={this.deleteItem}>
-                <i className="fa fa-trash-o" aria-hidden="true"></i>
-              </button>
-          </div>    
-        </div>
-      );
-    }
+  onCloseModal() {
+    this.setState({ open: false });
   }
 
-  export default TodoList;
+  onOpenModal() {
+    this.setState({ open: true });
+  }
+
+  markCompleted(event) {
+    this.props.onItemCompleted(this.props.id);
+  }
+  deleteItem(event) {
+    if (window.confirm('Are you sure you want to delete ?')) {
+      this.props.onDeleteItem(this.props.id);
+    }
+  }
+  handinfo(event) {
+    this.setState({
+      Isclick: true
+    });
+  }
+
+  CancleItem(event) {
+    this.setState({
+      Isclick: false,
+      text: this.state.oldtext
+    });
+  }
+
+  UpdateItem(event) {
+    event.preventDefault();
+    this.setState({
+      Isclick: false,
+      text: this.state.text,
+      oldtext: this.state.text,
+      des:this.state.des,
+      date:this.state.date
+    });
+
+    const Newupdate = {id:this.state.id,text: this.state.text,des:this.state.des,date:this.state.date}
+
+    this.props.onUpdateItem(this.props.id,Newupdate);
+  }
+
+  handleTextUpdate(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleDesUpdate(event) {
+    this.setState({
+      des: event.target.value
+    });
+  }
+
+  handleDateUpdate(event) {
+    this.setState({
+      date: event.target.value
+    });
+  }
+
+  render() {
+    var itemClass = "row form-check todoitem " + (this.props.completed ? "done" : "undone");
+    const { open } = this.state;
+    const duedate = moment(this.state.date).format("DD/MM/YYYY");
+    return (
+      <div id="box" className={itemClass} ref={li => this._listItem = li}>
+        <div className="col-md-1">
+          <input type="checkbox" className="form-check-input" onChange={this.markCompleted} checked={this.props.completed} />
+        </div>
+        <div className="col-md-9">
+          {this.state.Isclick === true ?
+            <form className="row" onSubmit={this.UpdateItem}>
+              <div className="col-md-8">
+                <input type="text" className="form-control" onChange={this.handleTextUpdate} value={this.state.text} placeholder="กรุณากรอกรายการ" />
+              </div>
+              <div className="col-md-4 btn-box">
+                <button disabled={!this.props.text} className="btn btn-primary">
+                  <i className='fa fa-check-circle-o' aria-hidden='true'></i>
+                </button>
+
+                <a onClick={this.CancleItem} className="btn btn-danger btn-paddingleft">
+                  <i className="fa fa-times-circle" aria-hidden="true"></i>
+                </a>
+              </div>
+            </form>
+            : <label className="form-check-label"><a onClick={this.handinfo} id="text_todo">{this.state.text}</a> <div className="duedate">{duedate}</div></label>}
+        </div>
+        <div className="col-md-1 nopaddingleft-right">
+          <button type="button" className="btn btn-success btn-sm" onClick={this.onOpenModal}>
+            <i className="fa fa-pencil-square-o"></i>
+          </button>
+        </div>
+        <div className="col-md-1 nopaddingleft-right" style={{ width: "35px" }}>
+          <button type="button" className="btn btn-danger btn-sm" onClick={this.deleteItem}>
+            <i className="fa fa-trash-o" aria-hidden="true"></i>
+          </button>
+        </div>
+        <Modal open={open} onClose={this.onCloseModal}>
+          <form className="submit_addtodo" onSubmit={this.UpdateItem}>
+            <div className="modal-header">
+              <h4 className="modal-title">Add New Todo</h4>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <label htmlFor="user">Title:</label>
+                <input type="text" className="form-control" onChange={this.handleTextUpdate} value={this.state.text} placeholder="Please input Title" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="des">Description:</label>
+                <textarea className="form-control" onChange={this.handleDesUpdate} value={this.state.des} placeholder="Please input Description">{this.state.des}</textarea>
+              </div>
+              <div className="form-group">
+                <label htmlFor="date">Date:</label>
+                <input type="date" className="form-control" onChange={this.handleDateUpdate} value={this.state.date} />
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-primary" disabled={!this.state.text}>Add</button>
+            </div>
+          </form>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
